bed57d149a920816aa06fa47dc7cc3b625f41049
MC11-projects <mariuscristianpopescu@yahoo.com>
2026-03-28 22:33:10 +0200

    Initial commit

---
+++ tests/auth.spec.js
@@ -0,0 +1,13 @@
+import { test, expect } from "allure-playwright";
+import dotenv from 'dotenv'
+import fs from 'fs'
+
+if (fs.existsSync('.env')) {
+    dotenv.config()
+}
+
+test('for initial commit', async ({page}) => {
+    const baseUrl = process.env.BASE_URL
+    await page.goto(baseUrl)
+    await expect(page.getByText('DailyDeals')).toBeVisible()
+})
